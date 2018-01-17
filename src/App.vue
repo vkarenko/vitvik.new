d18d9966d18f9d97389a76334bc3ba43cdc1826e
vkarenko <vitalii.karenko@me.com>
2018-01-17 13:12:12 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app" id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+  @import 'reset-css/reset.css'; // Eric Meyer's CSS reset
+
+  // Bootstrap
+  @import 'bootstrap/dist/css/bootstrap-grid.min.css';
+
+  // Base app styles
+  @import 'scss/app';
+</style>
